b8f813e1d4820f2dfd828fd52daa489f55ccc5be
bimalgtm <bimalg246@gmail.com>
2021-07-06 21:05:37 +0545

    modified

--- store/views.py
+++ store/views.py
@@ -7,12 +7,10 @@ def index(request):
     latest = Product.objects.filter(is_latest = True)
     exclusive = Product.objects.get(is_exclusive=True)
     featured = Product.objects.filter(is_featured=True)
-    all= Product.objects.filter(is_all=True)
     data ={
         'latest':latest,
         'exclusive':exclusive,
-        'featured':featured,
-        'all':all
+        'featured':featured
     }
     return render(request, "landing_page.html",data)
 
